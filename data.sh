f5d07d7887e142f599050f225247e9e7e60e0ab8
David Norris <x00046108@gmail.com>
2016-10-15 09:28:46 +0100

    week 3

---
+++ data.sh
@@ -0,0 +1,7 @@
+printf "What is you name? "
+read name
+printf "What month where you born? "
+read birthMonth
+printf "What is you favorite colour? "
+read colour
+printf "Hello $name you were born in $birthMonth and your favourite colour is $colour \n"
