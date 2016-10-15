23fd04b98a7feb3318781fcbc84d16068485df22
David Norris <x00046108@gmail.com>
2016-10-15 14:07:55 +0100

    week 4

---
+++ week4/dairy.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+read var
+echo date $var >> ~/scripts/week4/dairy.txt
---
+++ week4/pipe.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+ps -ef | grep mysql
+
+#ps - shows the curret processes, and the flag e selests all and f does full-format coloum lsiting
+
+#| - takes the output of the left command and uses it for an input for right command
+
+# grep searches the input of a command.
+
+
